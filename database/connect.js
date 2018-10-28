bd9fad019268f270dd12bccf478e58294f5e2479
Novata777 <isabelayarachi.7@gmail.com>
2018-10-27 22:03:03 -0400

    confogurando las advertencias de Desaprobacion ===Deprecation Warnings==

--- database/connect.js
+++ database/connect.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/ComidaRapida", { useNewUrlParser: true });
-
+mongoose.set('useFindAndModify', false);
 module.exports = mongoose;
