f6de3a9db9f46929a60af8aaf32635b72165c6f8
Novata777 <isabelayarachi.7@gmail.com>
2018-10-29 17:01:07 -0400

    instanciando los service menus

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var restauranteRouter = require('./routes/api/serviceRestaurants');
+var menuRouter = require('./routes/api/serviceMenus');
 
 var app = express();
 
@@ -24,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use('/api', restauranteRouter);
+app.use('/api/', restauranteRouter);
+app.use('/api/', menuRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
